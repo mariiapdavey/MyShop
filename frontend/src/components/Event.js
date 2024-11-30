59d16f80116e5d8b50f464cffee6cfc97214259b
mariiapdavey <mariiapdavey@gmail.com>
2024-11-29 21:21:36 -0800

    added Links to Event and Product

--- frontend/src/components/Event.js
+++ frontend/src/components/Event.js
@@ -4,12 +4,15 @@ import { Card } from 'react-bootstrap'
 const Event = ({event}) => {
   return (
     <Card className ='my-5 p-3 rounded'>
+      <Link to={`/product/${event._id}`}>
         <Card.Img src={event.image} variant ='top' />
-
+        </Link>
         <Card.Body>
+          <Link to={`/product/${event._id}`}>
             <Card.Title as='div'>
                 <strong>{event.name}</strong>
             </Card.Title>
+          </Link> 
             <Card.Text as='h3'>
                 ${event.price}
             </Card.Text>
